44e3be3593133259b6a108c778ea34ad12f47487
kareem05-ash <kareem.ash05@gmail.com>
2026-08-03 20:36:20 +0300

    add: include/accelerator_pkg.sv for cfg addr enum

---
+++ include/accelerator_pkg.sv
@@ -0,0 +1,9 @@
+package accelerator_pkg;
+    typedef enum logic [2:0] { 
+        IMG_WIDTH,
+        IMG_HEIGHT,
+        SHIFT_AMT,
+        RELU_EN,
+        ROUND_EN
+    } cfg_addr_t;
+endpackage : accelerator_pkg
