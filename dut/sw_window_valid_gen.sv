8bc6211b49909355b0f3fb7f15191e8e9c2cc7dc
kareem05-ash <kareem.ash05@gmail.com>
2026-08-05 00:00:35 +0300

    add-rtl: sw_window_valid sub module of sliding window

---
+++ dut/sw_window_valid_gen.sv
@@ -0,0 +1,17 @@
+module sw_window_valid_gen #(
+    // Parameters
+        parameter N = 3,
+        parameter IMG_WIDTH = 32
+) (
+    // Inputs
+        input  logic        pixel_valid,    // flags a valid pixel is being received
+        input  logic [$clog2(IMG_WIDTH)-1 : 0]  col_idx,
+        input  logic [$clog2(N)-1 : 0]          row_idx,
+    // Outputs
+        output logic        window_valid
+);
+    assign window_valid = 
+                pixel_valid         &&
+                (col_idx >= (N - 1))&&
+                (row_idx >= (N - 1));
+endmodule : sw_window_valid_gen
