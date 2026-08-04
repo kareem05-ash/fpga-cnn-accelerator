e74a9eba7b3f92eaf406e79a1dd2b8abff92f324
kareem05-ash <kareem.ash05@gmail.com>
2026-08-05 02:52:35 +0300

    mod-rtl: sw_window_valid_gen

--- dut/sw_window_valid_gen.sv
+++ dut/sw_window_valid_gen.sv
@@ -6,12 +6,12 @@ module sw_window_valid_gen #(
     // Inputs
         input  logic        pixel_valid,    // flags a valid pixel is being received
         input  logic [$clog2(IMG_WIDTH)-1 : 0]  col_idx,
-        input  logic [$clog2(N)-1 : 0]          row_idx,
+        input  logic [$clog2(N)-1 : 0]          stored_rows,
     // Outputs
         output logic        window_valid
 );
     assign window_valid = 
-                pixel_valid         &&
-                (col_idx >= (N - 1))&&
-                (row_idx >= (N - 1));
+                pixel_valid             &&
+                (col_idx     >= (N - 1))&&
+                (stored_rows == (N - 1));
 endmodule : sw_window_valid_gen
