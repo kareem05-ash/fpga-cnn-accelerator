a3ff9a9fe72851f2357def07d1eb74a6d9355e72
danaMohamed324 <dana125dmk@gmail.com>
2026-08-05 02:25:49 +0300

    Implement input_if.sv (registered passthrough for input synchronization)

---
+++ dut/input_if.sv
@@ -0,0 +1,22 @@
+module input_if(
+		input wire logic clk, rst, pixel_valid,processing_en,
+		input wire logic [7:0] pixel_in,
+		output  logic [7:0] pixel_out,
+		output  logic pixel_out_valid);
+		
+	always@(posedge clk, negedge rst) begin 
+		if(!rst) begin 
+			pixel_out<=0;
+			pixel_out_valid<=0;
+		end
+		else if (processing_en & pixel_valid) begin 
+			pixel_out<= pixel_in;
+			pixel_out_valid<= 1;
+		end
+		else begin 
+			pixel_out<=0;
+			pixel_out_valid<=0;
+		end
+	end
+	
+endmodule
