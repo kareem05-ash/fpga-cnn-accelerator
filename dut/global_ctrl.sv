fd9e3eb1a960c9f48996e12ad17081ce6a32553a
danaMohamed324 <dana125dmk@gmail.com>
2026-08-05 14:08:26 +0300

    Implement global_ctrl.sv and add state_t enum to accelerator_pkg.sv

--- dut/global_ctrl.sv
+++ dut/global_ctrl.sv
@@ -7,32 +7,32 @@ module global_ctrl(
 	
 	state_t cu_state, nx_state;
 					
-	always@(posedge clk, negedge rst) begin 
+	always@(posedge clk, negedge rst) begin   // REGISTER CU_STATE 
 		if(!rst) cu_state<=IDLE;
 		else cu_state<=nx_state;
 	end
 	
 	always@(*) begin 
-		processing_en=0;
+		processing_en=0;        // DEFAULT VALUES  
 		done=0;
 		busy=0;
 		case(cu_state) 
-			IDLE: begin 
+			IDLE: begin                   // STATE_1 WAIT FOR START SIGNAL 
 				if(start==1) nx_state=PROCESSING;
 				else nx_state=IDLE;
 			end
-			PROCESSING: begin 
+			PROCESSING: begin            // STATE_2 EN_PROCESSING AND BUSY  WAIT FOR DONE SIGNAL
 				processing_en=1;
 				busy=1;
 				if(output_done==1) nx_state=DONE;
 				else nx_state=PROCESSING;
 			end
-			DONE: begin 
+			DONE: begin                 // STATE_3 SEND DONE SIGNAL 
 				done=1;
 				nx_state=IDLE;
 			end
-			default: begin 
-				nx_state=cu_state;
+			default: begin             // DEFAULT RETURN TO IDLE 
+				nx_state=IDLE;
 			end
 		endcase
 	end
