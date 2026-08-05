b456ccc220d44bb1f639703fdac6ceecd8495868
danaMohamed324 <dana125dmk@gmail.com>
2026-08-05 14:04:06 +0300

    Implement MAC_array.sv (semi-parallel: N processing elements, row-select mux, row-sum buffer)

--- dut/MAC_array.sv
+++ dut/MAC_array.sv
@@ -8,15 +8,15 @@ module MAC #(parameter int N)(
  logic valid_out [0:N-1];
  logic signed [15:0] product [N-1:0];
 
- logic [$clog2(N)-1:0] row_sel;
+ logic [$clog2(N)-1:0] row_sel;   
 
- logic [7:0] window_row [0:N-1];
- logic signed [7:0] kernel_row [0:N-1];
+ logic [7:0] window_row [0:N-1];           //MUX_SEL_WINDOW_ROW
+ logic signed [7:0] kernel_row [0:N-1];    //MUX_SEL_KERNEL_ROW
 
  logic signed [16+$clog2(N)-1:0] partial_sum_comb;
  
 
- generate
+ generate                                  // N PROCESSING ELEMENT INSTANTIATION
   for (genvar r = 0; r < N; r++) begin
 	 processing_element U0 (.valid_in(window_valid), .pixel(window_row[r]), .coeff(kernel_row[r]), 
 		.valid_out(valid_out[r]), .product(product[r]));
@@ -24,7 +24,7 @@ module MAC #(parameter int N)(
  endgenerate 
 
 
- always@(*) begin 
+ always@(*) begin                         // ROW_SEL_LOGIC               
 	if ( window_valid) begin 
 		window_row=window[row_sel];
 		kernel_row=kernel_coeff[row_sel];
@@ -35,7 +35,7 @@ module MAC #(parameter int N)(
 	end
  end
 
- always@(*) begin 
+ always@(*) begin                        // SUM OF SEL ROW AFTER PROCESSING ELEMENT 
 	partial_sum_comb=0;
 	if(valid_out[row_sel]) begin 
 		for(int i=0;i<N;i++) begin 
@@ -44,15 +44,15 @@ module MAC #(parameter int N)(
 	end
  end
 
- always@(posedge clk , negedge rst) begin 
+ always@(posedge clk , negedge rst) begin         // INCREMENT ROW_SEL AND CALC PARTIAL_VALID SIGNAL
 	if(!rst) begin
 		row_sel<=0;
 		partial_valid<=0;
 	end
 	else if( valid_out[row_sel] ) begin 
-		partial_sum[row_sel]<=partial_sum_comb;
+		partial_sum[row_sel]<=partial_sum_comb;    // REGISTER PARTIAL SUM
 		if(row_sel!=N-1) begin 
-			row_sel<=row_sel+1;
+			row_sel<=row_sel+1;           
 			partial_valid<=0;
 		end
 		else begin 
