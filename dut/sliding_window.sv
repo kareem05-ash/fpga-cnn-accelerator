eef76a4e9f38e9bc9ffe2a0926894a01edbda5b3
kareem05-ash <kareem.ash05@gmail.com>
2026-08-05 03:07:40 +0300

    mod-rtl: sliding_window's been split into 3 sub modules
    1. sw_addr_manager: handles pointers, counters, addresses
    2. sw_window_valid_gen: responsible for window_valid signal
    3. sw_window_extractor: pure combinational block that produce the window[]

--- dut/sliding_window.sv
+++ dut/sliding_window.sv
@@ -1,7 +1,7 @@
 module sliding_window #(
     // Parameters
-        parameter   N = 3,
-        parameter   IMG_WIDTH
+        parameter   int N = 3,
+        parameter   int IMG_WIDTH
 )(
     // Inputs
         input  logic        clk,            // global active-high clk
@@ -12,93 +12,51 @@ module sliding_window #(
         output logic        window_valid,   // flags a valid window
         output logic [7:0]  window[N*N]     // active window
 );
-    // Pointers Maping in line_buf 1-D Array
-    //////////////////////////////////////////////////
-    // oldest   active  newest                  oldest
-    //          oldest  active  newest
-    //                  oldest  active  newest
-    // newest                   oldest  active  newest  
-    // active   newest                  oldest  active
-    //////////////////////////////////////////////////
     // 1-D array representing line buffers + active row
-        logic [7:0] line_buf [N * IMG_WIDTH];
-    
-    // Internal Signals
-        logic [$clog2(IMG_WIDTH)-1:0]       col_idx_cnt;
-        logic [$clog2(N * IMG_WIDTH)-1:0]   buf_wr_ptr;
-        logic [$clog2((N-1) * IMG_WIDTH):0] active_row_ptr;
-        logic [$clog2((N-1) * IMG_WIDTH):0] oldest_row_ptr;
-        logic [$clog2((N-1) * IMG_WIDTH):0] newest_row_ptr;
-
+    logic [7:0] line_buf [N * IMG_WIDTH];
+    logic [$clog2(N * IMG_WIDTH)-1 : 0] wr_addr;
+    logic [$clog2(IMG_WIDTH)-1 : 0]     col_idx;
+    logic [$clog2(N)-1 : 0] active_row,
+                            oldest_row,
+                            stored_rows;
     always_ff @(posedge clk) begin
-        if (!rst_n) begin
-            window_valid    <= '0;
-            col_idx_cnt     <= '0;
-            buf_wr_ptr      <= '0;
-            oldest_row_ptr  <= '0;
-            newest_row_ptr  <= (N - 2) * IMG_WIDTH;
-            active_row_ptr  <= (N - 1) * IMG_WIDTH;
-        // Normal Data Streaming
-        end else if (pixel_valid) begin
-            // Line Buffer Still Not Full
-            if (buf_wr_ptr < (active_row_ptr + N - 1)) begin
-                window_valid <= '0;     // we can't generate window so far
-                line_buf[buf_wr_ptr] <= pixel_in;
-                buf_wr_ptr <= buf_wr_ptr + 1;
-                if (buf_wr_ptr >= active_row_ptr)
-                    col_idx_cnt <= col_idx_cnt + 1;
-            // Now, we can generate window
-            end else begin
-                window_valid <= 1'b1;
-                // last pixel of that row
-                if (col_idx_cnt == IMG_WIDTH - 1) begin
-                    col_idx_cnt <= '0;
-                    active_row_ptr <= oldest_row_ptr;
-                    newest_row_ptr <= active_row_ptr;
-                    oldest_row_ptr <= (oldest_row_ptr == (N-1)*IMG_WIDTH)? '0 : oldest_row_ptr + IMG_WIDTH;
-                end else begin
-                    col_idx_cnt <= col_idx_cnt + 1;
-                end
-                // active row is one of the boundary rows
-                if (((active_row_ptr > oldest_row_ptr) && (active_row_ptr > newest_row_ptr)) || 
-                    ((active_row_ptr < oldest_row_ptr) && (active_row_ptr < newest_row_ptr))) begin
-                    // fill in the first N-1 rows of the window from oldest to newest
-                    for (shortint i = 0; i < N - 1; i++) begin
-                        // fill in N entry of each row
-                        for (shortint j = col_idx_cnt; j < col_idx_cnt + N; j++) begin
-                            window [(i * N) + j - col_idx_cnt] <= line_buf [oldest_row_ptr + (i * IMG_WIDTH) + j];
-                        end
-                    end
-                    // fill in the last row with active data
-                    for (shortint i = col_idx_cnt; i < (col_idx_cnt + N - 1); i++) begin
-                        window [((N - 1) * N) + i - col_idx_cnt] <= line_buf [active_row_ptr + i];
-                    end
-                    // fill in the last pixel with the pixel is being received
-                    window [(N * N) - 1] <= pixel_in;
-                    line_buf [active_row_ptr + col_idx_cnt] <= pixel_in;
-                // active row is between oldest and newest
-                end else begin
-                    // fill in the window from oldest to the end
-                    for (shortint i = 0; i < (N - (oldest_row_ptr/IMG_WIDTH)); i++) begin
-                        for (shortint j = col_idx_cnt; j < col_idx_cnt + N; j++) begin
-                            window [(i * N) + j - col_idx_cnt] <= line_buf [oldest_row_ptr + (i * IMG_WIDTH) + j];
-                        end
-                    end
-                    // fill in the window from the begin to newest
-                    for (shortint i = 0; i < (newest_row_ptr/IMG_WIDTH); i++) begin
-                        for (shortint j = col_idx_cnt; j < col_idx_cnt + N; j++) begin
-                            window [(i * N) + j - col_idx_cnt] <= line_buf [(i * IMG_WIDTH) + j];
-                        end
-                    end
-                    // fill in the window with the active row
-                    for (shortint i = col_idx_cnt; i < (col_idx_cnt + N - 1); i++) begin
-                        window [((N - 1) * N) + i - col_idx_cnt] <= line_buf [active_row_ptr + i];
-                    end
-                    // fill in the last pixel with the pixel is being received
-                    window [(N * N) - 1] <= pixel_in;
-                    line_buf [active_row_ptr + col_idx_cnt] <= pixel_in;
-                end // active row is between oldest and newest
-            end // window generation
-        end // pixel_valid
-    end // always_ff
+        if (pixel_valid)
+            line_buf [wr_addr] <= pixel_in;
+    end
+
+    sw_addr_manager #(
+        .N        (N /* default 3 */),
+        .IMG_WIDTH(IMG_WIDTH /* default 32 */)
+     ) u_addr_manager (
+        .clk        (clk),
+        .rst_n      (rst_n),
+        .pixel_valid(pixel_valid),
+        .col_idx    (col_idx),
+        .stored_rows(stored_rows),
+        .active_row (active_row),
+        .oldest_row (oldest_row),
+        .wr_addr    (wr_addr)
+    );
+
+    sw_window_valid_gen #(
+        .N        (N /* default 3 */),
+        .IMG_WIDTH(IMG_WIDTH /* default 32 */)
+     ) u_window_valid_gen (
+        .pixel_valid (pixel_valid),
+        .col_idx     (col_idx),
+        .stored_rows (stored_rows),
+        .window_valid(window_valid)
+    );
+
+    sw_window_extractor #(
+        .N        (N /* default 3 */),
+        .IMG_WIDTH(IMG_WIDTH /* default 32 */)
+     ) u_window_extractor (
+        .line_buf  (line_buf),
+        .pixel_in  (pixel_in),
+        .col_idx   (col_idx),
+        .active_row(active_row),
+        .oldest_row(oldest_row),
+        .window    (window)
+    );
 endmodule
