e294df96d7e1df4389912179e776818aac2410ef
kareem05-ash <kareem.ash05@gmail.com>
2026-08-06 21:31:01 +0300

    mod-rtl: sliding_window -> add last signal logic

--- dut/sliding_window.sv
+++ dut/sliding_window.sv
@@ -7,9 +7,11 @@ module sliding_window #(
         input  logic        clk,            // global active-high clk
         input  logic        rst_n,          // synch active-low rst_n
         input  logic        pixel_valid,    // flags a valid pixel is being received
+        input  logic        pixel_last,     // flags that the last pixel is being processed
         input  logic [7:0]  pixel_in,       // input pixel {data streaming}
     // Outputs
         output logic        window_valid,   // flags a valid window
+        output logic        window_last,    // flags the last window is generated
         output logic [7:0]  window[N*N]     // active window
 );
     // 1-D array representing line buffers + active row
@@ -19,9 +21,20 @@ module sliding_window #(
     logic [$clog2(N)-1 : 0] active_row,
                             oldest_row,
                             stored_rows;
+    // Writing in line buffers
     always_ff @(posedge clk) begin
         if (pixel_valid)
             line_buf [wr_addr] <= pixel_in;
+    end
+
+    // Window last signal logic
+    always_ff @(posedge clk) begin
+        if (!rst_n)
+            window_last <= 1'b0;
+        else if (pixel_last && pixel_valid)
+            window_last <= 1'b1;
+        else
+            window_last <= 1'b0;
     end
 
     sw_addr_manager #(
