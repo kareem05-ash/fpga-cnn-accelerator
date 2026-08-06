37300abe349e6d0982844433ad3c46a23281d950
kareem05-ash <kareem.ash05@gmail.com>
2026-08-06 21:41:30 +0300

    mod-rtl: relu -> add relu_last signal logic

--- dut/relu.sv
+++ dut/relu.sv
@@ -5,12 +5,15 @@ module relu #(
     // Inputs
         input  logic signed [ACC_W-1 : 0]   conv_result,    // convolution result from the accumulator
         input  logic                        conv_valid,     // flags a valid convolution result
+        input  logic                        conv_last,      // flags the last conv result is being processed
         input  logic                        relu_en,        // enables relu operation
     // Outputs
         output logic signed [ACC_W-1 : 0]   relu_result,    // ReLU result
-        output logic                        relu_valid      // flags a valid ReLU valid result 
+        output logic                        relu_valid,     // flags a valid ReLU valid result
+        output logic                        relu_last       // flags the last relu output is sent
 );
     assign relu_valid = conv_valid;
+    assign relu_last  = conv_valid && conv_last;
     always_comb begin
         // relu_en && conv_valid && conv_result is negative must asserted at the same time 
         if (relu_en && conv_valid && conv_result[ACC_W-1]) begin
