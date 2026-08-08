7b8922a532f50fddbc368a2bfdd1676561c0112c
danaMohamed324 <dana125dmk@gmail.com>
2026-08-09 01:17:31 +0300

    Update input_if.sv

--- dut/input_if.sv
+++ dut/input_if.sv
@@ -1,21 +1,27 @@
 module input_if(
-		input wire logic clk, rst, pixel_valid,processing_en,
-		input wire logic [7:0] pixel_in,
+		input  logic clk,
+		input  logic rst_n,
+		input  logic pixel_valid,
+		input  logic processing_en,
+		input  logic pixel_last,
+		input  logic [7:0] pixel_in,
 		output  logic [7:0] pixel_out,
+		output  logic pixel_out_last,
 		output  logic pixel_out_valid);
 		
-	always@(posedge clk, negedge rst) begin    // LOW ASYNCHRONOUS RST 
-		if(!rst) begin 
-			pixel_out<=0;
+	always_ff@(posedge clk) begin     
+		if(!rst_n) begin        // LOW SYNCHRONOUS RST 
 			pixel_out_valid<=0;
+			pixel_out_last<=0;
 		end
 		else if (processing_en & pixel_valid) begin   // PASS PIXEL & SEND VALID SIGNAL 
 			pixel_out<= pixel_in;
 			pixel_out_valid<= 1;
+			pixel_out_last  <= pixel_last;
 		end
-		else begin                         
-			pixel_out<=0;
+		else begin
 			pixel_out_valid<=0;
+			pixel_out_last<=0;
 		end
 	end
 	
