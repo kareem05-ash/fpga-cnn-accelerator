3ddfa53848d2eff8a86b72481994357d278bbbea
kareem05-ash <kareem.ash05@gmail.com>
2026-08-11 01:28:45 +0300

    mod-rtl: MAC_array -> fix indentation

--- dut/MAC_array.sv
+++ dut/MAC_array.sv
@@ -2,7 +2,7 @@ module MAC_array #(
     // Parameters
         parameter int N = 3,
         parameter int PROD_W = 17,                 // 8-bit unsigned * 8-bit signed = 17-bit signed
-	      parameter int ACC_W = PROD_W + $clog2(N)
+        parameter int ACC_W = PROD_W + $clog2(N)
 ) (
     // Inputs
         input  logic              clk,             // +ve edge triggered system clk
@@ -12,50 +12,50 @@ module MAC_array #(
         input  logic        [7:0] window [N*N],    // 1-D array of N*N pixels
         input  logic signed [7:0] kernel [N*N],    // 1-D kernel of N*N coeff's
     // Outputs
-        output logic              conv_valid,       // flags a valid conv_result
-        output logic              conv_last,        // flags that the conv_result is for the last output
+        output logic              conv_valid,      // flags a valid conv_result
+        output logic              conv_last,       // flags that the conv_result is for the last output
         output logic signed [ACC_W-1 : 0] conv_result
 );
 
-  logic valid_out [N*N-1 : 0];
-  logic signed [PROD_W-1 : 0] product [N*N-1 : 0];
-  logic signed [PROD_W+$clog2(N)-1 : 0] conv_result_comb;
- 
+    logic valid_out [N*N-1 : 0];
+    logic signed [PROD_W-1 : 0] product [N*N-1 : 0];
+    logic signed [PROD_W+$clog2(N)-1 : 0] conv_result_comb;
 
-  generate                                  // N*N PROCESSING ELEMENT INSTANTIATION
-    for (genvar r = 0; r < N*N; r++) begin
-	    processing_element #(
-				.PROD_W(PROD_W)
-				) U0 (
-				  .valid_in(window_valid), 
-			    .pixel(window[r]), 
-				  .coeff(kernel[r]), 
-				  .valid_out(valid_out[r]), 
-	  		  .product(product[r])
-			  );
+
+    generate                                  // N*N PROCESSING ELEMENT INSTANTIATION
+        for (genvar r = 0; r < N*N; r++) begin : GEN
+            processing_element #(
+                    .PROD_W(PROD_W)
+                    ) U0 (
+                    .valid_in(window_valid), 
+                    .pixel(window[r]), 
+                    .coeff(kernel[r]), 
+                    .valid_out(valid_out[r]), 
+                    .product(product[r])
+                );
+        end
+    endgenerate
+    
+    always_comb begin 
+        conv_result_comb = '0;
+        for(int i = 0; i < N*N; i++) begin 
+                if(valid_out[i])
+                    conv_result_comb += product[i];
+            end
     end
-  endgenerate
-  
-  always_comb begin 
-	  conv_result_comb = '0;
-      for(int i = 0; i < N*N; i++) begin 
-			  if(valid_out[i])
-				  conv_result_comb += product[i];
-		  end
-  end
 
-  always_ff @(posedge clk) begin         
-	  if (!rst_n) begin
-		  conv_valid  <= 0;
-		  conv_last   <= 0;
-	  end else if (window_valid) begin
-		  conv_result <= conv_result_comb;
-		  conv_valid  <= 1;
-		  conv_last   <= window_last;
-	  end else begin 
-		  conv_valid  <= 0;
-		  conv_last   <= 0;
-	  end
-  end
-  
+    always_ff @(posedge clk) begin         
+        if (!rst_n) begin
+            conv_valid  <= 0;
+            conv_last   <= 0;
+        end else if (window_valid) begin
+            conv_result <= conv_result_comb;
+            conv_valid  <= 1;
+            conv_last   <= window_last;
+        end else begin 
+            conv_valid  <= 0;
+            conv_last   <= 0;
+        end
+    end
+
 endmodule
