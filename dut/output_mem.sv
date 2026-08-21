b261a1eb8acf33b4574067eab286e4b52634152e
roaayousry <roaaousry66@gmail.com>
2026-08-21 03:01:58 +0300

    fix(output_mem): update output_mem RTL design

--- dut/output_mem.sv
+++ dut/output_mem.sv
@@ -1,30 +1,27 @@
-module output_mem #(parameter out_width = 32 , out_height = 32 , addr_width = $clog2((out_height)*(out_width)) , data_width = 16)
-(
-	input clk,
-	input rst_n,						//synchronous
-	input output_we,					//write enable
-	input output_re,					//read enable
-	input [addr_width-1:0] output_addr,
-	input [data_width-1:0] output_data,	//out from the output_ctl
-	output reg [out_width-1:0] read_data
+module output_mem # (
+    parameter int OUT_WIDTH = 32,
+    parameter int OUT_HEIGHT = 32,
+    parameter int DATA_W = 16,
+    parameter int DEPTH = OUT_WIDTH * OUT_HEIGHT,
+    parameter int ADDR_W = $clog2(DEPTH)
+)(
+    input  logic clk,                                   // active-high system clock
+    input  logic output_we,                      // 1: synch write | 0: asynch read
+    input  logic [DATA_W-1 : 0]      output_wdata,
+    input  logic [ADDR_W-1 : 0]      output_addr,
+    output logic [DATA_W-1 : 0]      output_rdata
 );
 
-reg [out_width-1:0] mem [0:(out_width*out_height)-1];		//every address refere to one pixel not one row
+logic [DATA_W-1:0] mem [0:(DEPTH)-1];		//every address refere to one pixel not one row
 
 always_ff @(posedge clk)
 	begin
-		if(!rst_n)
+		if (output_we)
 			begin
-				read_data <= 'd0;
-			end
-		else if (output_we && !output_re)				//in case both 1s preserve the values 
-			begin
-				mem[output_addr] <= output_data;
-			end
-		else if (!output_we && output_re)
-			begin
-				read_data <= mem[output_addr];
+				mem[output_addr] <= output_wdata;
 			end
 	end
+
+assign output_rdata = (!output_we) ? mem[output_addr] : 'd0;
 
 endmodule
