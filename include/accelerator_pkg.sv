33d6b406aedb9a012eaa3982d79fbc814ef22ebc
kareem05-ash <kareem.ash05@gmail.com>
2026-08-23 05:45:46 +0300

    add accelerator_pkg

---
+++ include/accelerator_pkg.sv
@@ -0,0 +1,13 @@
+package accelerator_pkg;
+    typedef enum logic [1:0] {
+        IDLE,
+        PROCESSING,
+        DONE
+    } state_t;
+
+    typedef enum logic [1:0] {
+        SHIFT_AMT,
+        RELU_EN,
+        ROUND_EN
+    } cfg_addr_t;
+endpackage : accelerator_pkg
