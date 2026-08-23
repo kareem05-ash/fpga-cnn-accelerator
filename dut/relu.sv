0027d81da0c76b4307c80e6d674d13485744fecc
kareem05-ash <kareem.ash05@gmail.com>
2026-08-23 05:40:54 +0300

    mod-rtl: relu -> remove (relu_en)

--- dut/relu.sv
+++ dut/relu.sv
@@ -8,7 +8,7 @@ module relu #(
         input  logic signed [ACC_W-1 : 0]   conv_result,    // convolution result from the accumulator
         input  logic                        conv_valid,     // flags a valid convolution result
         input  logic                        conv_last,      // flags the last conv result is being processed
-        input  logic                        relu_en,        // enables relu operation
+        // input  logic                        relu_en,        // enables relu operation
     // Outputs
         output logic signed [ACC_W-1 : 0]   relu_result,    // ReLU result
         output logic                        relu_valid,     // flags a valid ReLU valid result
@@ -18,7 +18,8 @@ module relu #(
     assign relu_last  = conv_valid && conv_last;
     always_comb begin
         // relu_en && conv_valid && conv_result is negative must asserted at the same time 
-        if (relu_en && conv_valid && conv_result[ACC_W-1]) begin
+        // if (relu_en && conv_valid && conv_result[ACC_W-1]) begin
+        if (conv_valid && conv_result[ACC_W-1]) begin
             // apply ReLU
             relu_result = '0;   // Pass zero instead of negative value
         end else begin
