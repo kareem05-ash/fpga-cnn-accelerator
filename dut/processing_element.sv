fc37d9d02feae52a046da36b50223d9944f7f211
kareem05-ash <kareem.ash05@gmail.com>
2026-08-29 04:21:24 +0300

    mod-rtl: PE -> rm valid signal logic

--- dut/processing_element.sv
+++ dut/processing_element.sv
@@ -5,10 +5,10 @@ module processing_element #(
     // Inputs
         input logic unsigned [7:0] pixel,   // input unsigned data pixel [0-255]
         input logic signed   [7:0] coeff,   // kernel signed coefficient [-128-127]
-        input logic                valid_in,// flags that input pixel is valid
+        // input logic                valid_in,// flags that input pixel is valid
     // Outputs
         output logic signed [PROD_W-1 : 0] product, // product signed output
-        output logic               valid_out// flags that product is valid iff valid_in asserted
+        // output logic               valid_out// flags that product is valid iff valid_in asserted
 );
     // product_dsp signal is needed to infer a dsp blcok for each PE
     (* use_dsp48 = "yes" *)
@@ -18,5 +18,5 @@ module processing_element #(
     assign pixel_ext    = {1'b0, pixel};
     assign product_dsp  = pixel_ext * coeff;
     assign product      = product_dsp;
-    assign valid_out    = valid_in;
+    // assign valid_out    = valid_in;
 endmodule
