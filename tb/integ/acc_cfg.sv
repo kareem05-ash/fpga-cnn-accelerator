a65331a769ebb6775e99c7c8d22bb9ed2165d5b5
kareem05-ash <kareem.ash05@gmail.com>
2026-09-09 05:17:10 +0300

    mod: acc_cfg.sv -> add vif & stimulus_sent_e

--- tb/integ/acc_cfg.sv
+++ tb/integ/acc_cfg.sv
@@ -1,22 +1,23 @@
 package acc_cfg_pkg;
-
-	import uvm_pkg::*;
 	`include "uvm_macros.svh"
+	import uvm_pkg::*;
 	
 	class acc_cfg extends uvm_object;
-		
 		`uvm_object_utils(acc_cfg)
-		
-		uvm_active_passive_enum  is_active;
-		event read_in_kernel;
-		event busy_state;
-		event read_in_processing;
-		event processing_done;
+
+		virtual acc_if acc_vif;
+
+		// uvm_active_passive_enum  is_active;
+    
+		// event read_in_kernel;
+		// event busy_state;
+		// event read_in_processing;
+		// event processing_done;
+
+    event stimulus_sent_e;
 		
 		function new (string name = "acc_cfg");
 			super.new(name);
 		endfunction
-		
 	endclass
-	
 endpackage
